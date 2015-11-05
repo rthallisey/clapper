deb91e9f1e3c7332c005ca498f1c6bc79cf59b34
Tomas Sedovic <tomas@sedovic.cz>
2015-11-05 16:25:52 +0100

    Add the basic validation routes

--- ansible-tests/validations-api.py
+++ ansible-tests/validations-api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -8,7 +8,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return jsonify({"msg": "Hello World!"})
+
+
+@app.route('/v1/validations/')
+def list_validations():
+    return jsonify({"TODO": "List existing validations"})
+
+
+@app.route('/v1/validations/<uuid>/')
+def show_validation(uuid):
+    return jsonify({
+        'uuid': uuid,
+        'TODO': "return validation info",
+    })
+
+
+@app.route('/v1/validations/<uuid>/run', methods=['PUT'])
+def run_validation(uuid):
+    return jsonify({
+        'TODO': "run the given validation",
+    })
 
 
 app.run(debug=True)
