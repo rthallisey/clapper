e874e98c95c26381056aacc731b24193800b8670
Tomas Sedovic <tomas@sedovic.cz>
2015-11-05 17:05:52 +0100

    Use a custom response renderer
    This lets us specify the error codes and make sure we always return
    json.

--- ansible-tests/validations-api.py
+++ ansible-tests/validations-api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, abort, jsonify
+from flask import Flask, abort, json, make_response
 
 import validations
 
@@ -8,9 +8,17 @@ import validations
 app = Flask(__name__)
 
 
+def json_response(code, result):
+    # NOTE: flask.jsonify doesn't handle lists, so we need to do it manually:
+    response = make_response(json.dumps(result), code)
+    response.headers['Content-Type'] = 'application/json'
+    response.code = code
+    return response
+
+
 @app.route('/')
 def index():
-    return jsonify({"msg": "Hello World!"})
+    return json_response(200, {"TODO": "Print the API routes."})
 
 
 @app.route('/v1/validations/')
@@ -21,14 +29,14 @@ def list_validations():
         'name': validation['name'],
     }
     for validation in validations.get_all().values()]
-    return jsonify({'validations': result})
+    return json_response(200, result)
 
 
 @app.route('/v1/validations/<uuid>/')
 def show_validation(uuid):
     try:
         validation = validations.get_all()[uuid]
-        return jsonify({
+        return json_response(200, {
             'uuid': validation['uuid'],
             'ref': '/v1/validations/' + validation['uuid'],
             'name': validation['name'],
@@ -36,14 +44,19 @@ def show_validation(uuid):
             'results': [],
         })
     except KeyError:
-        abort(404)
+        return json_response(404, {})
 
 
 @app.route('/v1/validations/<uuid>/run', methods=['PUT'])
 def run_validation(uuid):
-    return jsonify({
-        'TODO': "run the given validation",
-    })
+    try:
+        validation = validations.get_all()[uuid]
+        # TODO: this blocks. Run it in the background
+        results = validations.run(validation)
+        #return json_response(204, results)
+        return json_response(200, results)
+    except KeyError:
+        return json_response(404, {})
 
 
 app.run(debug=True)
