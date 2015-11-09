c0a6734fac7ec2d377e98d2e20234a74db309c91
Tomas Sedovic <tomas@sedovic.cz>
2015-11-09 10:14:04 +0100

    Store the validation results
    Just use a global variable for now. In a real setup, this should live in
    a database.

--- ansible-tests/validations-api.py
+++ ansible-tests/validations-api.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-from flask import Flask, abort, json, make_response
+from flask import Flask, abort, json, make_response, url_for, request
 
 import validations
+
+
+DB_VALIDATIONS = {}  # TODO: OMG THREAD SAFETY
 
 
 app = Flask(__name__)
@@ -25,7 +28,7 @@ def index():
 def list_validations():
     result = [{
         'uuid': validation['uuid'],
-        'ref': '/v1/validations/' + validation['uuid'],
+        'ref': url_for('show_validation', uuid=validation['uuid']),
         'name': validation['name'],
     }
     for validation in validations.get_all().values()]
@@ -34,14 +37,16 @@ def list_validations():
 
 @app.route('/v1/validations/<uuid>/')
 def show_validation(uuid):
+    global DB_VALIDATIONS
     try:
         validation = validations.get_all()[uuid]
+        db_validation = DB_VALIDATIONS.get(uuid, {})
+
         return json_response(200, {
             'uuid': validation['uuid'],
-            'ref': '/v1/validations/' + validation['uuid'],
-            'name': validation['name'],
-            'status': 'new',
-            'results': [],
+            'ref': url_for('show_validation', uuid=uuid),
+            'status': db_validation.get('status', 'new'),
+            'results': db_validation.get('results', []),
         })
     except KeyError:
         return json_response(404, {})
@@ -49,10 +54,20 @@ def show_validation(uuid):
 
 @app.route('/v1/validations/<uuid>/run', methods=['PUT'])
 def run_validation(uuid):
+    global DB_VALIDATIONS
     try:
         validation = validations.get_all()[uuid]
+        db_validation = DB_VALIDATIONS.setdefault(uuid, {})
+        db_validation['status'] = 'running'
         # TODO: this blocks. Run it in the background
         results = validations.run(validation)
+        # TODO: add timestamp to the results
+        success = all((result.get('success') for result in results.values()))
+        if success:
+            db_validation['status'] = 'success'
+        else:
+            db_validation['status'] = 'failed'
+        db_validation.setdefault('results', []).append(results)
         #return json_response(204, results)
         return json_response(200, results)
     except KeyError:
