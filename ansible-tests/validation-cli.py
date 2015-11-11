74a1af5a48ffa37053dcffde8683c93d0155c7d9
Martin André <m.andre@redhat.com>
2015-11-11 17:49:32 +0900

    Add results command to cli

--- ansible-tests/validation-cli.py
+++ ansible-tests/validation-cli.py
@@ -17,6 +17,22 @@ def command_list(**args):
     for validation in resp.json():
         print "%s. %s" % (validation['uuid'], validation['name'])
 
+def command_results(**args):
+    resp = requests.get(VALIDATION_SERVER + 'validation_results/')
+    if resp.status_code != 200:
+        die('something went wrong')
+    for result in resp.json():
+        print "%s: %s" % (result['date'], result['status'])
+        if 'detailed_description' in result:
+            for (host, detail) in result['detailed_description'].items():
+                print "\t%s: skipped=%s\tok=%s\tchanged=%s\tunreachable=%s\tfailed=%s" % \
+                    (host,
+                     detail['skipped'],
+                     detail['ok'],
+                     detail['changed'],
+                     detail['unreachable'],
+                     detail['failures'])
+
 def command_run(*args):
     if len(args) != 1:
         die("You must pass one argument: the validation ID.")
