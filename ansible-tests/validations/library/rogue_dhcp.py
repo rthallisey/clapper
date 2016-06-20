b160241c1021c4cefdf9bdf44fd7613cc32f16af
Tomas Sedovic <tomas@sedovic.cz>
2016-06-20 07:40:02 -0400

    Install scapy into a virtualenv
    Some systems (e.g. RHEL 7) don't ship pip, which means the Rogue DHCP
    validation could not install its dependencies. This changes the validation to
    create a virtualenv and install scapy there.
    
    Unfortunately, I couldn't find a way to have Ansible modules include this new
    virtualenv, so this change includes rewriting rogue_dhcp.py to be a standalone
    script executed within the virtualenv using the `command` Ansible module
    instead of treating it as a normal module.
    
    That means we lose some of the finer-grained reporting capabilities. In
    addition, it looks like our PoC API (validation-api.py) doesn't report STDERR
    or anything useful in situations like this -- that will be addressed in the
    follow-up commits.

--- ansible-tests/validations/library/rogue_dhcp.py
+++ ansible-tests/validations/library/rogue_dhcp.py
@@ -1,27 +1,12 @@
 #!/usr/bin/env python
 
-from ansible.module_utils.basic import *
-from scapy.all import *
+# Disable scapy's warning to stderr:
+import logging
+import sys
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-DOCUMENTATION = '''
----
-module: rogue_dhcp
-short_description: Detect rogue DHCP servers running on the specified networks.
-author: "Tomas Sedovic, @tomassedovic"
-requirements:
-- the "scapy" Python module
-options:
-  networks:
-    description:
-    - List of network ranges in CIDR notation to validate the discovered DHCP
-      servers against.
-    required: true
-  timeout_seconds:
-    description:
-    - The amount of time in seconds to wait for DHCP responses.
-    required: false
-    default: 30
-'''
+
+from scapy.all import *
 
 
 def find_dhcp_servers(timeout_sec):
@@ -32,43 +17,20 @@ def find_dhcp_servers(timeout_sec):
                      UDP(sport=68, dport=67) /
                      BOOTP(chaddr=hw) /
                      DHCP(options=[("message-type", "discover"), "end"]))
-    ans, unans = srp(dhcp_discover, multi=True, timeout=timeout_sec)
+    ans, unans = srp(dhcp_discover, multi=True, timeout=timeout_sec, verbose=False)
 
     return [(unicode(packet[1][IP].src), packet[1][Ether].src)
             for packet in ans]
 
 
 def main():
-    module = AnsibleModule(
-        argument_spec={
-            'networks': dict(required=True, type='list'),
-            'timeout_seconds': dict(default=30, type='int'),
-        }
-    )
-
-    # TODO: validate the specified networks against the discovered DHCP server
-    networks = module.params.get('networks')
-    if not networks:
-        module.exit_json(changed=False, msg='No networks were specified.')
-
-    dhcp_servers = find_dhcp_servers(module.params.get('timeout_seconds'))
-    warnings = []
-    msg = "No DHCP servers found."
-    changed = False
+    dhcp_servers = find_dhcp_servers(30)
     if dhcp_servers:
-        msg = 'Found %d DHCP servers.' % len(dhcp_servers)
-        changed = True
-        warnings.append(msg)
-        formatted_servers = ("%s (%s)" % (ip, mac) for (ip, mac) in dhcp_servers)
-        # TODO: write out the networks when we actually use them for anything:
-        warnings.extend(formatted_servers)
-
-    result = {
-        'changed': changed,
-        'msg': msg,
-        'warnings': warnings,
-    }
-    module.exit_json(**result)
+        sys.stderr.write('Found %d DHCP servers:' % len(dhcp_servers))
+        sys.stderr.write("\n".join(("* %s (%s)" % (ip, mac) for (ip, mac) in dhcp_servers)))
+        sys.exit(1)
+    else:
+        print "No DHCP servers found."
 
 
 if __name__ == '__main__':
